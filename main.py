8e7a5a717960c2e23d8ac6e62ca6f1515eab2d4b
Matheus Souza <rodriguessouzamatheus@hotmail.com>
2024-06-27 11:48:43 -0300

    2.6

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ fundo = pygame.image.load("recursos/fundo.png")
 fundoStart = pygame.image.load("recursos/fundoStart.png")
 fundoDead = pygame.image.load("recursos/fundoDead.png")
 
-
+praca = pygame.image.load("recursos/praca.jpg")
+praca = pygame.transform.scale(praca, (100, 75))
 skol = pygame.image.load("recursos/inimigo.png")
 brahma = pygame.image.load("recursos/inimigo2.png")
 tamanho = (800,600)
@@ -31,9 +32,6 @@ preto = (0, 0 ,0 )
 amarelo = (255, 255, 0)
 
 
-
-praca = pygame.image.load("recursos/praca.jpg")
-praca = pygame.transform.scale(praca, (100, 75))
 def jogar(nome):
     #pygame.mixer.Sound.play(missileSound)
     #pygame.mixer.music.play(-1)
@@ -95,23 +93,16 @@ def jogar(nome):
         tela.blit(fundo, (0,0) )
         #pygame.draw.circle(tela, preto, (posicaoXPersona,posicaoYPersona), 40, 0 )
         tela.blit( personagemPrincipal, (posicaoXPersona, posicaoYPersona))
-        pygame.draw.circle(tela, amarelo, (750, 50), radius)
-        if aumentando:
-            radius += 0.15
-            if radius >=40:
-                aumentando = False
-            else:
-                radius -=0.15
-                if radius <=10:
-                    aumentando = True
+        
         posicaoYMissel = posicaoYMissel + velocidadeMissel
         if posicaoYMissel > 600:
             posicaoYMissel = -240
             pontos = pontos + 1
             velocidadeMissel = velocidadeMissel + 1
             posicaoXMissel = random.randint(0,800)
-            tela.blit( skol, (posicaoXMissel, posicaoYMissel) ) 
-            posicaoYMisselb = posicaoYMisselb + velocidadeMisselb
+            
+        tela.blit( skol, (posicaoXMissel, posicaoYMissel) ) 
+            
         
         posicaoYMisselb = posicaoYMisselb + velocidadeMisselb
         if posicaoYMisselb > 600:
@@ -121,9 +112,8 @@ def jogar(nome):
             posicaoXMissel = random.randint(0,800)
             #pygame.mixer.Sound.play(missileSound)
             
-            
-        tela.blit( skol, (posicaoXMissel, posicaoYMissel) )  
         tela.blit( brahma, (posicaoXMisselb, posicaoYMisselb) )
+        
         texto = fonte.render(nome+"- Pontos: "+str(pontos), True, branco)
         tela.blit(texto, (10,10))
         
@@ -134,27 +124,40 @@ def jogar(nome):
         pixelsMisselXb = list(range(posicaoXMisselb, posicaoXMisselb + larguaMisselb))
         pixelsMisselYb = list(range(posicaoYMisselb, posicaoYMisselb + alturaMisselb))
         #print( len( list( set(pixelsMisselX).intersection(set(pixelsPersonaX))   ) )   )
+        
         if  len( list( set(pixelsMisselY).intersection(set(pixelsPersonaY))) ) > dificuldade:
             if len( list( set(pixelsMisselX).intersection(set(pixelsPersonaX))   ) )  > dificuldade:
                 dead(nome, pontos)
+        
         if  len( list( set(pixelsMisselYb).intersection(set(pixelsPersonaY))) ) > dificuldade:
             if len( list( set(pixelsMisselXb).intersection(set(pixelsPersonaX))   ) )  > dificuldade:
                 dead(nome, pontos)
 
-                posicaoYpraca += movimentoYpraca
-                if posicaoXpraca <= 0 or posicaoXpraca >= tamanho[0] - 50:
-                    movimentoXpraca = - movimentoXpraca
+        posicaoYpraca += movimentoYpraca
+        posicaoXpraca += movimentoXpraca
+        if posicaoXpraca <= 0 or posicaoXpraca >= tamanho[0] - 50:
+         movimentoXpraca = - movimentoXpraca
                 
-                if posicaoYpraca <= 0 or posicaoYpraca >= tamanho[1] - 50:
-                    movimentoYpraca = - movimentoYpraca
+        if posicaoYpraca <= 0 or posicaoYpraca >= tamanho[1] - 50:
+         movimentoYpraca = - movimentoYpraca
 
-                tela.blit(praca, (posicaoXpraca, posicaoYpraca))
-                
+        tela.blit(praca, (posicaoXpraca, posicaoYpraca))
+
+
+        pygame.draw.circle(tela, amarelo, (750, 50), radius)
+        if aumentando:
+          radius += 0.15
+          if radius >=40:
+             aumentando = False
+        else:
+          radius -=0.15
+          if radius <=10:
+           aumentando = True        
         
         pygame.display.update()
         relogio.tick(60)
 
-
+#joia
 def dead(nome, pontos):
     pygame.mixer.music.stop()
     #pygame.mixer.Sound.play(explosaoSound)
